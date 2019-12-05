e37d9c5f456ce4e0148ea411d76e54d3266242b4
rikoefendi <lismiyani.riko@gmail.com>
2019-12-05 19:39:23 +0700

    add cache

--- src/GDrive.js
+++ src/GDrive.js
@@ -1,5 +1,7 @@
 const request = require('request')
-var extend = require('extend')
+const extend = require('extend')
+const crypto = require('crypto')
+const fs = require('fs')
 const userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
 const host = 'drive.google.com'
 let baseUri = `https://${host}`
@@ -11,7 +13,7 @@ const defHeaders = {
   referer: baseUri
 }
 
-const Graber = function(id, headers, callback = null) {
+const Graber = function (id, headers, callback = null) {
   const params = initParams(id, headers, callback)
   this.id = params.id
   this.callback = params.callback
@@ -30,8 +32,41 @@ Graber.prototype.defaultOpt = {
   followAllRedirect: false,
 }
 
+Graber.prototype.cache = function (fileId, link = '') {
+  const path = './tmp/drivecache/'
+  let hash = crypto.createHash('md5').update(fileId).digest("hex")
+  const expired = new Date().getTime() + 1000 * 60 * 5
+  const toCache = expired + '~' + link
+
+  if (!fs.existsSync(path)) fs.mkdirSync(path)
+  if (!fs.existsSync(path + hash)) {
+    if (link) {
+      fs.writeFile(path + hash, toCache, (a, b) => {
+        console.log('created');
+      })
+    }
+    return null
+  }
+  const ready = fs.readFileSync(path + hash)
+  if (ready) {
+    const data = ready.toString()
+    const datas = data.split('~')
+    if (datas[0] > (new Date).getTime()) {
+      return datas[1]
+    } else {
+      fs.unlink(path + hash, (a, b) => {
+        console.log('deleted');
+      })
+    }
+  }
+
+}
 
 Graber.prototype.parseLink = function (opt) {
+  const cache = this.cache(this.id)
+  if (cache) {
+    return this.callback(false, cache)
+  }
   if (this.reloaded > 3) {
     return this.callback(true, 'max excecution time')
   }
@@ -57,8 +92,8 @@ Graber.prototype.parseLink = function (opt) {
     if (!redirect) {
       return this.parseLink(opt)
     }
-    this.callback(null, redirect)
-    return
+    this.cache(this.id, redirect)
+    return this.callback(null, redirect)
   })
 }
 
@@ -81,6 +116,7 @@ function initParams(id, headers, callback) {
 }
 
 function GDrive(id, headers, callback) {
-  return new Graber(id,headers, callback)
+  return new Graber(id, headers, callback)
 }
 module.exports = GDrive
+
